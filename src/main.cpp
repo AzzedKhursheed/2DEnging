1b732d4c5c9f4feaf187af100037beba0b0e7cd1
Azzed Khursheed <azzedkhursheed47@gmail.com>
2025-07-12 19:25:49 -0500

    Fromatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,44 @@
-// OpenGL loader (GLAD) and context/window management (GLFW)
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
-
-// Standard libraries for I/O and math
-#include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <fstream> // For file operations
-#include <vector> // For std::vector
-
-// ImGui headers
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
 
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) 
+{
   glViewport(0, 0, width, height);
 }
 
+void loop(GLFWwindow *window)
+{
+    while(!glfwWindowShouldClose(window))
+    {
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    }
+}
 
 int main()
 {
-    if (!glfwInit()) 
-    {  
+    if (!glfwInit())
+    {
     std::cout << "Failed to initialize GLFW" << std::endl;
     return -1;
     }
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "2DEnging", NULL, NULL);
-    
-    if (!window) 
+
+    if (!window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -41,7 +48,7 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    if (!gladLoaderLoadGL()) 
+    if (!gladLoaderLoadGL())
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         glfwDestroyWindow(window);
@@ -50,6 +57,8 @@ int main()
     }
 
     glEnable(GL_DEPTH_TEST);
+
+    loop(window);
+
+    return 1;
 }
-
-
